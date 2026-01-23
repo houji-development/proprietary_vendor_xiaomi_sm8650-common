a97e00de186f5296058b67a88610392cac190a84
lolipuru <banstellazz@gmail.com>
2026-01-23 12:53:11 +0000

    test

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -1432,6 +1432,7 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element.xml \
     android.hardware.security.keymint-service-qti.xml \
     atfwd-saidl.xml \
+    bttpi-saidl.xml \
     c2_manifest_vendor.xml \
     c2_manifest_vendor_audio.xml \
     dataconnection-saidl.xml \
